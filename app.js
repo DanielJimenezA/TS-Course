e622e315fa4e9c49231ae0fc3d7f622be285ded0
DanielJimenezA <djimeneza1400@alumno.ipn.mx>
2022-12-14 00:10:26 -0600

    Observable Mode
    Transpilar el código TS en tiempo real

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
-var msg = 'Hola mundo';
+"use strict";
+let msg = 'Hola mundo';
 console.log(msg);
+const hero = {
+    name: 'Batman',
+    age: 45
+};
+hero.age = 50;
+console.log(hero.age + 1);
--- app.ts
+++ app.ts
@@ -1,2 +1,11 @@
 let msg:string = 'Hola mundo';
 console.log(msg);
+
+const hero = {
+    name: 'Batman',
+    age:45
+}
+
+hero.age = 50;
+
+console.log(hero.age+1);
