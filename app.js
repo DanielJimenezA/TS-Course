c57866e83a49f34b8dc23acff74644852049c128
DanielJimenezA <djimeneza1400@alumno.ipn.mx>
2022-12-15 22:34:22 -0600

    Función anónima

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 "use strict";
-let msg = 'Hola mundo';
-console.log(msg);
-const hero = {
-    name: 'Batman',
-    age: 45
-};
-hero.age = 50;
-console.log(hero.age + 1);
+// let msg:string = 'Hola mundo';
+// console.log(msg);
+// const hero = {
+//     name: 'Batman',
+//     age:45
+// }
+// //hero.age = '50'; No es posible asignar un string a un number, 
+// //error que si marca TS
+// hero.age = 50;
+// console.log(hero.age+1);
+const a = 10; // es un tipo número que no puede cambiar
+let b = 10; // es un tipo número que puede cambiar
+const c = 10; // es un tipo numérico
+let d = 10; // es un tipo numérico
+b = 3.1416;
+// b={};       //No se pueden asignar valores diferentes ya que el tipo de 
+// b=[];    //variable es un número
+// b=true;
+function sayHello(msg) {
+    console.log(msg + '' + 'Daniel');
+    msg.charAt(0);
+}
+//Función anónima
+(() => {
+    const e = 19;
+    console.log(a);
+})();
--- app.ts
+++ app.ts
@@ -1,11 +1,36 @@
-let msg:string = 'Hola mundo';
-console.log(msg);
 
-const hero = {
-    name: 'Batman',
-    age:45
+// let msg:string = 'Hola mundo';
+// console.log(msg);
+
+// const hero = {
+//     name: 'Batman',
+//     age:45
+// }
+
+// //hero.age = '50'; No es posible asignar un string a un number, 
+// //error que si marca TS
+
+// hero.age = 50;
+
+// console.log(hero.age+1);
+const a = 10; // es un tipo número que no puede cambiar
+let b = 10; // es un tipo número que puede cambiar
+
+const c:number = 10; // es un tipo numérico
+let d:number = 10; // es un tipo numérico
+
+b = 3.1416;
+// b={};       //No se pueden asignar valores diferentes ya que el tipo de 
+// b=[];    //variable es un número
+// b=true;
+
+function sayHello(msg: string){ // Al darle el tipo string vamos a tener los métodos del tipo string
+    console.log(msg + '' + 'Daniel');
+    msg.charAt(0);
 }
 
-hero.age = 50;
-
-console.log(hero.age+1);
+//Función anónima
+(()=>{
+    const e:number = 19;
+    console.log(a);
+})()
