565f991502e5550ea6b9ac117fc75dccbfe360b6
lAbEl JD <83861199+DanielJimenezA@users.noreply.github.com>
2023-01-11 00:36:49 -0600

    Booleans
    Uso de booleanos en TS

---
+++ tipos/booleans.js
@@ -0,0 +1,8 @@
+"use strict";
+(() => {
+    let isSuperman = true;
+    let isBatman = false;
+    //console.log({isSuperman}) //Haciendo uso de las llaves podemos saber el valor de la variable
+    isSuperman = true && false;
+    console.log({ isSuperman });
+})();
---
+++ tipos/booleans.ts
@@ -0,0 +1,9 @@
+(()=>{
+    let isSuperman:boolean = true;
+    let isBatman:boolean = false;
+
+    //console.log({isSuperman}) //Haciendo uso de las llaves podemos saber el valor de la variable
+
+    isSuperman = true && false;
+    console.log({isSuperman})
+})()
