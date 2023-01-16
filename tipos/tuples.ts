00f55635d0150518e264c2b08e19d71e8a6efd03
lAbEl JD <83861199+DanielJimenezA@users.noreply.github.com>
2023-01-15 23:13:36 -0600

    Tuplas
    Uso de Tuplas en TS.

---
+++ tipos/tuples.ts
@@ -0,0 +1,10 @@
+(()=>{
+
+    const hero:[string,number, boolean] = ['Dr. Strange', 100, true];
+    hero[0] = 'Ironman'; 
+    hero[1] = 50; 
+    hero[2] = false; 
+
+    //Podemos agregar definiciones de tuplas a una variable
+
+})()
