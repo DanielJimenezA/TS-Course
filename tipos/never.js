4d8eb6beccbb95ac8c03f3e96db1db53ae99367d
lAbEl JD <83861199+DanielJimenezA@users.noreply.github.com>
2023-01-16 21:43:30 -0600

    Never
    Uso de tipo Never en TS.

---
+++ tipos/never.js
@@ -0,0 +1,13 @@
+"use strict";
+(() => {
+    const error = (message) => {
+        //never significa que nunca va a terminar de ejecutarse exitosamente
+        //regresa un never o un number
+        if (false) {
+            throw new Error(message);
+        }
+        return 1;
+    };
+    error('Auxilio!');
+    console.log('Hola Mundo!');
+})();
---
+++ tipos/never.ts
@@ -0,0 +1,16 @@
+(()=>{
+
+    const error = (message:string):(never|number) => {
+    //never significa que nunca va a terminar de ejecutarse exitosamente
+    //regresa un never o un number
+    if(false){
+        throw new Error(message);
+    }
+    return 1;
+
+    }
+
+    error('Auxilio!');
+    console.log('Hola Mundo!');
+
+})()
