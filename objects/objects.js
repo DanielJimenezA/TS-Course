d46151a519c085e35e8d610b193a304b7df70377
lAbEl JD <83861199+DanielJimenezA@users.noreply.github.com>
2023-01-23 00:41:30 -0600

    Objetos
    Uso de Objetos en TS.

---
+++ objects/objects.js
@@ -0,0 +1,19 @@
+"use strict";
+(() => {
+    //Este es un objeto al cual se le pueden agregar propiedades con diferentes tipos de datos
+    //y TS lo infiere
+    let flash = {
+        name: "Barry Allen",
+        age: 24,
+        powers: ["Speed", "Time Travel", "Dimensional Travel"]
+    };
+    //Al utilizar nuestros objetos en funciones, podemos definir el tipo de dato que reciben
+    flash = {
+        name: "Clark Kent",
+        age: 500,
+        powers: ["Super Strength", "Super Speed", "Flight", "Heat Vision", "Freeze Breath"]
+        // getNombre(){
+        //     return this.name;
+    };
+});
+();
