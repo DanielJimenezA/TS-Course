4d15ed64016363e28b620a961f567ae257f68b4c
lAbEl JD <83861199+DanielJimenezA@users.noreply.github.com>
2023-01-24 23:51:14 -0600

    Propiedad Function en objetos
    Uso de propiedad del tipo Function opciona y con salida de argumentos de tipo string

--- objects/objects2.ts
+++ objects/objects2.ts
@@ -2,9 +2,10 @@
     //Este es un objeto al cual se le pueden agregar propiedades con diferentes tipos de datos
     //y TS lo infiere
 
-    //En los objetos en TS, no importa el orden en el que se escriban las propiedades aún
-    //cuando sean de tipo opcional, p.ej. name?:string
-    let flash: {name:string, age:number, powers:string[]} = {
+    //En los objetos en TS, no importa el orden en el que se escriban las propiedades aún cuando sean de tipo opcional, p.ej. name?:string
+    
+    //Agregamos una propiedad opcional con el signo de interrogación llamada getNombre del tipo Function y que tiene un retorno de tipo string
+    let flash: {name:string, age:number, powers:string[], getNombre?:()=> string} = {
         name: "Barry Allen",
         age: 24,
         powers: ["Speed", "Time Travel", "Dimensional Travel"]
@@ -13,10 +14,10 @@
     flash = {
         name: "Clark Kent",
         age: 500,
-        powers: ["Super Strength", "Super Speed", "Flight", "Heat Vision", "Freeze Breath"]
-        // getNombre((){
-        //     return this.name;
-        // }))
+        powers: ["Super Strength", "Super Speed", "Flight", "Heat Vision", "Freeze Breath"],
+        getNombre(){
+            return this.name;
+        }
     }
 
     
