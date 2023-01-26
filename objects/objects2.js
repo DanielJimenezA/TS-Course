46aec547cc8355b4afb0de44bd001a9c469d4c53
lAbEl JD <83861199+DanielJimenezA@users.noreply.github.com>
2023-01-25 23:55:45 -0600

    Tipos personalizados
    Uso de tipos personalizados en objetos en TS.

--- objects/objects2.js
+++ objects/objects2.js
@@ -2,8 +2,8 @@
 (() => {
     //Este es un objeto al cual se le pueden agregar propiedades con diferentes tipos de datos
     //y TS lo infiere
-    //En los objetos en TS, no importa el orden en el que se escriban las propiedades aún
-    //cuando sean de tipo opcional, p.ej. name?:string
+    //En los objetos en TS, no importa el orden en el que se escriban las propiedades aún cuando sean de tipo opcional, p.ej. name?:string
+    //Agregamos una propiedad opcional con el signo de interrogación llamada getNombre del tipo Function y que tiene un retorno de tipo string
     let flash = {
         name: "Barry Allen",
         age: 24,
@@ -12,9 +12,9 @@
     flash = {
         name: "Clark Kent",
         age: 500,
-        powers: ["Super Strength", "Super Speed", "Flight", "Heat Vision", "Freeze Breath"]
-        // getNombre((){
-        //     return this.name;
-        // }))
+        powers: ["Super Strength", "Super Speed", "Flight", "Heat Vision", "Freeze Breath"],
+        getNombre() {
+            return this.name;
+        }
     };
 })();
---
+++ objects/type.ts
@@ -0,0 +1,20 @@
+(() => {
+
+    type Heroe = {
+
+        
+    }
+
+    let flash: {name:string, age:number, powers:number[], getNombre?:()=> string} = {
+        name: "Barry Allen",
+        age: 24,
+        powers: ["1", "2", "3"]
+    };
+    
+    let superman: {name:string, age:number, powers:string[], getNombre?:()=> string} = {
+        name: "Barry Allen",
+        age: 24,
+        powers: ["Speed"]
+    };
+})();
+
