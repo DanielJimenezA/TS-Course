f536a9ce8e04024762ed7002e44e88b8813a7b9e
lAbEl JD <83861199+DanielJimenezA@users.noreply.github.com>
2023-02-07 23:58:15 -0600

    ES6
    Se recomienda utilizar ES6

--- ES6/let-vars.ts
+++ ES6/let-vars.ts
@@ -3,10 +3,11 @@
     let a = "Daniel";
     console.log(a);
 
-if(true){
-    console.log(a);
+function getName() {
+    console.log('Viejo getName');
 }
 
-console.log("Esto es un console.log")
-console.log("Esto es un commit")
+getName = () => {
+    console.log('Nuevo getName')};
+getName();
 })()
--- main.js
+++ main.js
@@ -2,5 +2,12 @@
 (() => {
     let a = "Daniel";
     console.log(a);
+    function getName() {
+        console.log('Viejo getName');
+    }
+    getName = () => {
+        console.log('Nuevo getName');
+    };
+    getName();
 })();
 //# sourceMappingURL=main.js.map
