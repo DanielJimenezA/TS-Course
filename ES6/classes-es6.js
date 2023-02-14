1e498eed6d44d863dd6607f9ad7d03b3e1e03981
lAbEl JD <83861199+DanielJimenezA@users.noreply.github.com>
2023-02-13 23:56:16 -0600

    Update classes-es6.js
    En JS no tenemos control sobre las propiedades de una clase ya que se pueden agregar indiscriminadamente sin ser necesariamente utilizadas.

--- ES6/classes-es6.js
+++ ES6/classes-es6.js
@@ -11,10 +11,25 @@
             //Establecemos los valores de las propiedades
             this.name = name;
             this.power = power;
-        }    
-}
+        }
+    }
+    //Con la palabra reservada extends podemos heredar de una clase
+    class FlyingAvenger extends Avenger {
+        flying; //No sabemos el tipo, sólo podemos especificarlo en el constructor
+        constructor(name, power) {
+            super(name, power); //Llamamos al constructor de la clase padre
+            this.flying = true; //Es un ejemplo en el que podemos establecer el tipo
+        }
+    }
+    const hulk = new Avenger('Hulk', 1500);
+    const falcon = new FlyingAvenger('Falcon', 500);
+    
+    console.log(hulk);
+    console.log(falcon);
 
-const hulk = new Avenger('Hulk', 1500);
-console.log(hulk);
+    //El super constructor es el constructor de la clase padre
 
-}) ();
+    //Nota: En JS no tenemos control sobre las propiedades de una clase ya que se
+    //pueden agregar indiscriminadamente sin ser utilizadas realmente, en TS sí
+
+})();
